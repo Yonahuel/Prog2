787cc31d0287f3110cd26ad791f5bc71de7431bd
matucalvo <matucalv@gmail.com>
2023-03-28 12:37:57 -0300

    punto 5

--- tpRecursividad/muestrapunto5recu.c
+++ tpRecursividad/muestrapunto5recu.c
@@ -1,16 +1,16 @@
 #include "punto5recu.c"
 
 int main(){
-    float n,m, resultado;
+    int dividendo,divisor, resultado;
 
-    printf("Ingrese un numero m: \n");
-    scanf("%f", &m);
-    printf("Ingrese un numero n: \n");
-    scanf("%f", &n);
+    printf("Ingrese un dividendo: \n");
+    scanf("%d", &dividendo);
+    printf("Ingrese un divisor: \n");
+    scanf("%d", &divisor);
 
-    resultado = restoRecursivo(m,n);
+    resultado = restoRecursivo(dividendo,divisor);
 
-    if (resultado != -1) {printf("El cociente de estos numeros es: %.4f", resultado);}
+    printf("El cociente de estos numeros es: %d", resultado);
 
 
 }
--- tpRecursividad/punto5recu.c
+++ tpRecursividad/punto5recu.c
@@ -3,17 +3,23 @@
 #include <stdio.h>
 #include "TP1_Recursividad.h"
 
-float restoRecursivo(float m, float n){
+int restoRecursivo(int dividendo, int divisor){
 
-    float resultado = 0;
 
-    if (n == 0){
-        printf("Error no se puede dividir por cero \n");    // si n es 0 termina ahi
-        resultado = -1;
-    } else if (m<n){        // cuando m sea menor que n va a terminar de restar y llamarse a si misma
-        resultado = 0; 
-    } else resultado = 1 + restoRecursivo(m-n, n);  // si m es mayor, hace la recursividad restando n a m
-    
+    int resultado = 0;
+    int signo = 1;
+
+    if (divisor == 0){                                                          // si divisor es 0 tira error
+        printf("Error no se puede dividir por cero \n");      
+    } else if (dividendo < 0){                                           // si el dividendo es negativo, guarda el signo y despues lo convierte en positivo para hacer la resta
+        signo = -signo; 
+        resultado = -restoRecursivo(-dividendo, divisor);    
+    } else if (divisor < 0){                                             // si el divisor es negativo, guarda el signo y despues lo convierte en positivo para hacer la resta
+        signo = -signo;
+        resultado = -restoRecursivo(dividendo, -divisor);
+    } else if (dividendo < divisor){                                   // caso base, cuando el dividendo sea menor que el divisor para la resta
+     resultado = dividendo;
+    } else resultado = signo * restoRecursivo(dividendo-divisor, divisor);          // recursion, multiplicando el signo que corresponde 
 
     return resultado;
            
